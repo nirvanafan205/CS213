88aa43cbe2f9813023144eba23043233b713ada3
roymck <lilmayo495@gmail.com>
2019-09-03 10:18:01 -0700

    update

---
+++ homework/LAB01.java
@@ -0,0 +1,42 @@
+public class LAB01 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Matthew Garcia: Lab 01");
+
+		int nbr1, nbr2, sum;
+
+		nbr1 = 5;
+		nbr2 = 7;
+		sum = nbr1 + nbr2;
+
+		System.out.println("nbr1 is " + nbr1);
+		System.out.println("nbr2 is " + nbr2);
+		System.out.println
+
+			if(sum % 2 == 1)
+			{
+				System.out.println("Odd");
+			}
+			else if(sum % 2 == 0)
+			{
+				System.out.println("Even");
+			}
+
+		nbr1 = 8;
+		sum = nbr1 + nbr2;
+
+		System.out.println("nbr1 is " + nbr1);
+		System.out.println("nbr2 is " + nbr2);
+		System.out.println
+
+			if(sum % 2 == 1)
+			{
+				System.out.println("Odd");
+			}
+			else if(sum % 2 == 0)
+			{
+				System.out.println("Even");
+			}
+	}
+}
