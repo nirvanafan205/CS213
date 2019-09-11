e29734efd0f3c0758bf10b75dd15619a7b579277
Matthew <mgarcia495@yahoo.com>
2019-09-10 20:59:28 -0700

    exercise 9

--- Princeton_Java/loops.java
+++ Princeton_Java/loops.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class loops
 {
+	/*
 	static void exercise_1()
 	{
 		System.out.println("Enter three numbers");
@@ -11,7 +12,7 @@ public class loops
 		int nber_one = nmbrs.nextInt();
 		int nber_two= nmbrs.nextInt();
 		int nber_three = nmbrs.nextInt();
-		
+
 		if(nber_one == nber_two && nber_one == nber_three && nber_two == nber_three)
 		{
 			System.out.println("They are all equal");
@@ -22,8 +23,25 @@ public class loops
 		}
 
 	}
+	*/
+
+	static void exercise_9()
+	{
+		for(int i = 1000; i < 2001; i++)
+		{
+			int first = i / 1000;
+			int second = (i / 100) % 10;
+			int third = (i / 10) % 100  % 10;
+			int fourth = i % 10;
+
+			System.out.println(first + " " + second + " " + third + " " + fourth);
+		}
+	}
+
 	public static void main(String[] args)
 	{
-		exercise_1();
+	//	exercise_1();
+
+		exercise_9();
 	}
 }
