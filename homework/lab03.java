1236465f15bcdd3d32c74dc4b4cb347a8bdde4c1
roymck <lilmayo495@gmail.com>
2019-09-11 20:50:04 -0700

    updated

--- homework/lab03.java
+++ homework/lab03.java
@@ -1,29 +1,35 @@
 public class lab03
 {
 
-	static void concatName(String firstName, String lastName)
+	static String concatName(String firstName, String lastName)
 	{
 		return firstName + " " + lastName;
 	}
 
 	static void checkAge(int check)
 	{
+
 		if(check >= 18)
 		{
+			System.out.println("You are " + check + "years old.");
+
 			System.out.println("You can vote");
 		}
 		else
 		{
+			System.out.println("You are " + check + "years old.");
+
 			System.out.println("You can't vote");
 		}
 	}
-	public static void main(String[] args)
+
+	public static void main(String args[])
 	{
 		System.out.println("Student Name: Lab03");
+	
+		concatName(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 
-		concatName();
-
-		checkAge();
+		checkAge()Integer.parseInt(args[2]);
 	}
 }
 
