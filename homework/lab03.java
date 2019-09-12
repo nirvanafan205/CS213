062a2a1e091f35bc74e0c2057ba3e4ecbf91144d
Matthew <mgarcia495@yahoo.com>
2019-09-11 23:32:08 -0700

    updated

--- homework/lab03.java
+++ homework/lab03.java
@@ -1,37 +1,37 @@
 public class lab03
 {
 
-        static void concatName(String firstName, String lastName)
-        {
-                System.out.printl("Hello " + firstName + " " + lastName);
-        }
+	static void concatName(String firstName, String lastName)
+	{
+		System.out.println("Hello " + firstName + " " + lastName);
+	}
 
-        static void checkAge(int check)
-        {
-                if(check >= 18)
-                {
-                        System.out.println("You are " + check + "years old.");
+	static void checkAge(int check)
+	{
+		if(check >= 18)
+		{
+			System.out.println("You are " + check + "years old.");
 
-                        System.out.println("You can vote");
-                }
-                else
-                {
-                        System.out.println("You are " + check + "years old.");
+			System.out.println("You can vote");
+		}
+		else
+		{
+			System.out.println("You are " + check + "years old.");
 
-                        System.out.println("You can't vote");
-                }
-        }
+			System.out.println("You can't vote");
+		}
+	}
 
-        public static void main(String args[])
-        {
-                System.out.println("Student Name: Lab03");
+	public static void main(String args[])
+	{
+		System.out.println("Student Name: Lab03");
 
-                firstName = args[0];
-                lastName = args[1];
-                check = args[3];
+		String firstName = args[0];
+		String lastName = args[1];
+		int check = Integer.parseInt(args[3]);
 
-                concatName(firsName, lastName);
+		concatName(firstName, lastName);
 
-                checkAge(check);
-        }
+		checkAge(check);
+	}
 }
