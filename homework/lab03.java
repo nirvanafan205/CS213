9721474753f8c14ec9d19b6ed006e0b0ff9b6b93
roymck <lilmayo495@gmail.com>
2019-09-11 20:33:09 -0700

    updated

--- homework/lab03.java
+++ homework/lab03.java
@@ -1,8 +1,29 @@
 public class lab03
 {
+
+	static void concatName(String firstName, String lastName)
+	{
+		return firstName + " " + lastName;
+	}
+
+	static void checkAge(int check)
+	{
+		if(check >= 18)
+		{
+			System.out.println("You can vote");
+		}
+		else
+		{
+			System.out.println("You can't vote");
+		}
+	}
 	public static void main(String[] args)
 	{
-		System.out.println("Hello WOrld");
+		System.out.println("Student Name: Lab03");
+
+		concatName();
+
+		checkAge();
 	}
 }
 
