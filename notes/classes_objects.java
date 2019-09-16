1042e1f942782f1f8a78c430c35d94cbe950fb78
roymck <lilmayo495@gmail.com>
2019-09-16 13:39:39 -0700

    multiple classes

--- notes/classes_objects.java
+++ notes/classes_objects.java
@@ -6,11 +6,16 @@ public class classes_objects
 	{
 		System.out.println("Hello WOrld");
 
-	// to creat an object of classes_objects, specify the class name, followed by the object name, and use the keyword 'new'
-	
-		classes_objects myObj = new classes_objects();
-		System.out.println(classes_objects.x);
+		// to creat an object of classes_objects, specify the class name, followed by the object name, and use the keyword 'new'
 
+		classes_objects myObj = new classes_objects(); // this is object 1
+
+		//multiple objects can be made in one class
+		classes_objects myObj2 = new classes_objects() // this is object 2
+
+		System.out.println(myObj.x);
+		// both will be prinint 5
+		System.out.println(myObj2.x);
 	}
 }
 
