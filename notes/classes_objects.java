90904fa2cdc3ce4b69ef634561319cd902a46b36
roymck <lilmayo495@gmail.com>
2019-09-16 13:32:17 -0700

    class made

--- notes/classes_objects.java
+++ notes/classes_objects.java
@@ -1,8 +1,16 @@
 public class classes_objects
 {
+	int x = 5;
+
 	public static void main(String[] args)
 	{
 		System.out.println("Hello WOrld");
+
+	// to creat an object of classes_objects, specify the class name, followed by the object name, and use the keyword 'new'
+	
+		classes_objects myObj = new classes_objects();
+		System.out.println(classes_objects.x);
+
 	}
 }
 
