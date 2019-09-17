763a05e86fe2b83512e010d50b92e00d52274204
roymck <lilmayo495@gmail.com>
2019-09-16 18:55:24 -0700

    working

--- notes/constructors.java
+++ notes/constructors.java
@@ -1,8 +1,24 @@
 public class constructors
+// A constructor is a special method that is used to initialize objects
+// The constructor is called when an object of a class is created
+// It can be used to set initial values for object attributes
+
+int x; // attribute is created
 {
+	// constructors must match the class name
+	// cannot have a return type
+
+	public constructors()
+	{
+		x = 666; // initial value is created for the class attribute x
+	}
+
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		constructors myObj = new constructors();
+
+		System.out.println(myObj.x); // prints x value
 	}
 }
+
 
