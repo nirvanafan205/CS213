bc6f37601dd667a7b4f07f197c58cfbb0e8c8edb
roymck <lilmayo495@gmail.com>
2019-09-16 17:37:40 -0700

    attributtes

--- notes/attributes.java
+++ notes/attributes.java
@@ -1,8 +1,17 @@
 public class attributes
 {
+	int x = 5;
+	// x and why are attributes of the class
+	// they are variables within a class
+	int y = 3;
+
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		// attributes can be accessed by createing an object of the class
+		// and by using the dot syntax(.)
+
+		attributes myObj = new attributes();
+
+		System.out.println(myObj.x);
 	}
 }
-
