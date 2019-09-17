5ffb13cb6cfd8a81d5105045d6101f491615a157
roymck <lilmayo495@gmail.com>
2019-09-16 18:58:44 -0700

    fixed

--- notes/constructors.java
+++ notes/constructors.java
@@ -5,20 +5,18 @@ public class constructors
 	// It can be used to set initial values for object attributes
 
 	int x; // attribute is created
+	// constructors must match the class name
+	// cannot have a return type
+
+	public constructors()
 	{
-		// constructors must match the class name
-		// cannot have a return type
+		x = 666; // initial value is created for the class attribute x
+	}
 
-		public constructors()
-		{
-			x = 666; // initial value is created for the class attribute x
-		}
+	public static void main(String[] args)
+	{
+		constructors myObj = new constructors();
 
-		public static void main(String[] args)
-		{
-			constructors myObj = new constructors();
-
-			System.out.println(myObj.x); // prints x value
-		}
+		System.out.println(myObj.x); // prints x value
 	}
 }
