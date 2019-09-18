05ad7ec1165eae898fdf2e3f6e04b978009c6994
roymck <lilmayo495@gmail.com>
2019-09-18 10:05:16 -0700

    asking a question

--- practice/classes.java
+++ practice/classes.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class classes
 {
+	static void asking (int number)
+	{
+		System.out.println("What is your favorite number?");
+	}
+
+
 	public static void main(Stringp[] args)
 	{
 		System.out.println("Hello World");
+
+		Scanner myObj = new Scanner(System.in);
+
+		int nmbr = myObj.nextInt();
+
+		asking(nmbr);
+
 	}
 }
 
