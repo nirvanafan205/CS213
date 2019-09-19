cd8126457a15c3c70c2b4e4a2c4d73d599a0e8be
roymck <lilmayo495@gmail.com>
2019-09-18 20:56:17 -0700

    name

--- practice/Lab04.java
+++ practice/Lab04.java
@@ -2,6 +2,9 @@ public class Lab04
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		System.out.println("Matthew Garcia: Lab04");
+
+		Car myObj = Car();
+
 	}
 }
