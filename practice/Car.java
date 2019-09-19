f8c1f719a4b730e4efc217a586acc0c608017b5f
roymck <lilmayo495@gmail.com>
2019-09-18 20:17:31 -0700

    privates added

---
+++ practice/Car.java
@@ -0,0 +1,18 @@
+public class Car
+{
+	private car_brand;
+	private model;
+	private gas_milage;
+	private gas_tank;
+	
+		public String carType()
+		{
+			return car_brand;
+		}
+
+		public void official_name(String naming)
+		{
+			this.car_brand = naming;
+		}
+
+}
