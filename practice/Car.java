5edb3af0b667d8230fc49ba17ba7105526190672
roymck <lilmayo495@gmail.com>
2019-09-18 20:38:41 -0700

    all sets/gets

--- practice/Car.java
+++ practice/Car.java
@@ -1,17 +1,56 @@
 public class Car
 {
 	private String car_brand;
-	
+	private String car_model;
+	private String car_gas;
+	private String car_gasTank;
+
 	// getter
-		public String getCar_brand()
-		{
-			return car_brand;
-		}
+	public String getCar_brand()
+	{
+		return car_brand;
+	}
 
 	// setter
-		public void setBrand_name(String naming)
-		{
-			this.car_brand = naming;
-		}
+	public void setBrand_name(String naming)
+	{
+		this.car_brand = naming;
+	}
+
+	// getter
+	public String getCar_model()
+	{
+		return car_model;
+	}
+
+	// setter
+	public void setCar_type(String model)
+	{
+		this.car_model = model;
+	}
+
+	// getter
+	public String getCar_gas()
+	{
+		return car_gas;
+	}
+
+	// setter
+	public void setCar_milage(String milage)
+	{
+		this.car_brand = mileage;
+	}
+
+	// getter
+	public String getCar_gasTank()
+	{
+		return car_gasTank;
+	}
+
+	// setter
+	public void setBrand_name(String tank)
+	{
+		this.car_brand = tank;
+	}
 
 }
