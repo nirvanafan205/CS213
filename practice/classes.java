116c1c4d1b8b6dd5f3f2da13ea38d079d8b0c4b6
roymck <lilmayo495@gmail.com>
2019-09-25 17:01:05 -0700

    erased hello

--- practice/classes.java
+++ practice/classes.java
@@ -4,14 +4,14 @@ public class classes
 {
 	int x = 5;
 	int y;
-	int z = x + y;
+	int z;
 
 	static int asking (int number)
 	{
 		return number;
 	}
 
-	public void car(String name)
+	public String car(String name)
 	{
 		return name;
 	}
@@ -24,23 +24,24 @@ public class classes
 
 		classes nameObj = new classes();
 
+		System.out.println("What is your favorite number?");
+
 		int nmbr = myObj.nextInt();
 
-		System.out.println("What is your favorite number?");
+		nameObj.y = nmbr;
 
 		System.out.println(asking(nmbr));
 
-		System.out.println("What is your dream car?");
+		System.out.println("\n" + "What is your dream car?");
 
 		String car = myObj.next();
 
-		System.out.println(nameObj.car(car));
+		System.out.println(nameObj.car(car) + "\n");
 
 		System.out.println("Enter a number m8");
+		
+		nameObj.z = nmbr + 10;
 
-		y = nextInt();
-
-		System.out.println(myObj.z);
-
+		System.out.println("Your favorite number has been added);
 	}
 }
