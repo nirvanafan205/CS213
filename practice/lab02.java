fa80c1cdea9a120cb58e1e3dc4e4f1860b97dd0a
roymck <lilmayo495@gmail.com>
2019-09-30 19:11:43 -0700

    lab02.java

--- practice/lab02.java
+++ practice/lab02.java
@@ -2,17 +2,22 @@ public class lab02
 {
 	public static void main(String[] args)
 	{
-		int[][] 2darray = { {1, 2, 3, 4, 5}
-			{6, 7, 8, 9, 10}
-			{11, 12, 13, 14, 15}
+		int[][] darray = { {1, 2, 3, 4, 5},
+			{6, 7, 8, 9, 10},
+			{11, 12, 13, 14, 15},
 		}
 
 		int subtotal, total;
 		String oddeven;
 
-		for(int i = 0; i < 2darray.length; i++)
+		for(int i = 0; i < darray.length; i++)
 		{
-			System.out.println(i);
+			System.out.println("Row: " + i);
+
+			for(int j = 0; j < darray.size; i++)
+			{
+				System.out.println(darray[i][j]);
+			}
 		}
 
 	}
