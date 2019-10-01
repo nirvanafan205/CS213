b8609100759281e1e8151e5fb23b8523c1e99f02
nirvanafan205 <47827017+nirvanafan205@users.noreply.github.com>
2019-09-30 18:47:43 -0700

    Update LAB01.java

--- homework/LAB01.java
+++ homework/LAB01.java
@@ -2,7 +2,7 @@ public class LAB01
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Matthew Garcia: Lab 01");
+		System.out.println("Matthew Garcia: Lab 01 \n ");
 
 		int nbr1, nbr2, sum;
 
@@ -12,15 +12,18 @@ public class LAB01
 
 		System.out.println("nbr1 is " + nbr1);
 		System.out.println("nbr2 is " + nbr2);
-		System.out.println("");
 
 		if(sum % 2 == 1)
 		{
-			System.out.println("Odd");
+		    System.out.println("sum is" + sum);
+			System.out.println("The sum is odd");
+			System.out.println("");
 		}
 		else if(sum % 2 == 0)
 		{
-			System.out.println("Even");
+		    System.out.println("sum is " + sum);
+			System.out.println("The sum is even");
+			System.out.println("");
 		}
 
 		nbr1 = 8;
@@ -28,15 +31,18 @@ public class LAB01
 
 		System.out.println("nbr1 is " + nbr1);
 		System.out.println("nbr2 is " + nbr2);
-		System.out.println("");
 
 		if(sum % 2 == 1)
 		{
-			System.out.println("Odd");
+			System.out.println("sum is " + sum);
+			System.out.println("The sum is odd");
+			System.out.println("");
 		}
 		else if(sum % 2 == 0)
 		{
-			System.out.println("Even");
+			 System.out.println("sum is " + sum);
+			System.out.println("The sum is even");
+			System.out.println("");
 		}
 	}
 }
