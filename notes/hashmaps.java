a4d95920eb53b171d77dc58807d19ebf847c781c
roymck <lilmayo495@gmail.com>
2019-10-22 12:33:07 -0700

    albums and songs

--- notes/hashmaps.java
+++ notes/hashmaps.java
@@ -1,7 +1,22 @@
+import java.util.HashMap; // to be able to use hashmaps, must import it
+// hashmaps are stores items in 'key/value' pairs. can be accessed by amother index type
+// can have string/integer types and values
+// I.e String "string value" 
+// int 0100110
 class hasmaps
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		//Created a HashMap object called Millermac
+
+		HashMap<String, String> Millermac = HashMap<String, String>();
+		// Adding keys and Values (album, song)
+
+		Millermac.put("GO:OD AM", "Perfect Circle / God SPeed");
+		Millermac.put("Swimming", "Self Care");
+		Millermac.put("Blue Side Park", "Missed Calls");
+		Millermac.put("Macadelic", "Clarity");
+
+		System.out.println(Millermac);
 	}
 }
