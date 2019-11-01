ca4557543a26d6b5042503bdbf5f4123325e105c
roymck <lilmayo495@gmail.com>
2019-11-01 10:32:18 -0700

    added

--- notes/Lab09.java
+++ notes/Lab09.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Lab09
 {
-	public static void main(String[] args);
+	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		System.out.println("Matthew Garcia: Lab 9");
+
+		HashMap<String, Integer> information = new HashMap<String, INteger>();
+		System.out.println("How many students do you want to enter?");
+		Scanner myObj = new Scanner(System.in);
+
+		int nmbr_students = myObj.nextInt();
+
+		System.out.println("How many scores do you want to enter for each student?");
+
+
 	}
 }
 
